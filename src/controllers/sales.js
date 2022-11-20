f1ea4ae23f3203e2f4b13c5e953a61a4bf8ce4c0
MichaelFranklyn <mfranklyn@outlook.com>
2022-11-20 09:31:50 -0300

    mod query sales

--- src/controllers/sales.js
+++ src/controllers/sales.js
@@ -97,7 +97,8 @@ const listSales = async (req, res) => {
       )
       .from("sales as s")
       .leftJoin("sellers as se", "se.id", "s.id_seller")
-      .leftJoin("cars as c", "c.id", "s.id_car");
+      .leftJoin("cars as c", "c.id", "s.id_car")
+      .orderBY("s.id");
 
     return res.status(200).json(salesData);
   } catch (error) {
